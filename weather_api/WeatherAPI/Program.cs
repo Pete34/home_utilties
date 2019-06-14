823db106ff0a888c094e9645dbcf1ae6b3dae5be
Peter Grady <notes4pete@gmail.com>
2019-06-14 15:04:02 -0500

    fixing health check

--- weather_api/WeatherAPI/Program.cs
+++ weather_api/WeatherAPI/Program.cs
@@ -1,27 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace WeatherAPI
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace WeatherAPI {
+    public class Program {
+        public static void Main(string[] args) {
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-            .UseKestrel(options =>
-            {
-                options.ListenAnyIP(5000);
+            .UseKestrel(options => {
+                options.ListenAnyIP(1971);
             })
             .UseStartup<Startup>();
     }
